281efb8401a4d05ff4440b69e10851938fc234dc
Becca <95663074+Acanthodoris@users.noreply.github.com>
2022-03-17 12:40:41 -0700

    fixing contect form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,40 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/Contact.css";
-import {
-  Form,
-  Button,
-} from "react-bootstrap";
+import { Form, Button, } from "react-bootstrap";
+import { validateEmail } from "../utils/helpers";
 
 function Contact() {
+  const [email, setEmail] = useState("");
+  const [fullName, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    // TODO: Add an else statement to the end that will set the password to the value of 'inputValue'
+
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "Name") {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !fullName) {
+      setErrorMessage("Something is not quite right. Please check your email and leave a name. ~Thank you!");
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+
+    // If successful, we want to clear out the input after registration.
+    setName("");
+    // TODO: Set the password back to an empty string after the user clicks submit
+    setMessage("");
+    setEmail("");
+    alert(`Hello ${fullName}`);
+  };
+
   return (
     <section class="Contact">
       <div className="container form">
         <div className="row">
           <div className="col-lg-10 bg-dark mx-auto text-white mb-4">
             <Form>
-            <Form.Group
-                className="mb-2"
-                controlId="exampleForm.ControlInput1"
-              >
+            <Form.Group className="mb-2" controlId="formFullName">
                 <Form.Label>Your Name:</Form.Label>
-                <Form.Control type="fullname" placeholder="First LastName" />
+                <Form.Control type="text" placeholder="First LastName" value={fullName} onChange={handleInputChange} />
               </Form.Group>
-              <Form.Group
-                className="mb-2"
-                controlId="exampleForm.ControlInput1"
-              >
+              <Form.Group className="mb-2">              
                 <Form.Label>Email address:</Form.Label>
-                <Form.Control type="email" placeholder="your.email@here.com" />
+                <Form.Control type="email" placeholder="your.email@here.com" value={email} onChange={handleInputChange}/>
               </Form.Group>
-              <Form.Group
-                className="mb-2"
-                controlId="exampleForm.ControlTextarea1"
-              >
+              <Form.Group className="mb-2" >
                 <Form.Label>Message:</Form.Label>
-                <Form.Control as="textarea" rows={3} />
+                <Form.Control as="textarea" rows={3} onChange={handleInputChange} value={message} />
               </Form.Group>
-              <Button variant="primary">Submit</Button>{" "}
+              <Button variant="primary" onClick={handleFormSubmit}>Submit</Button>{" "}
             </Form>
+            {errorMessage && (
+              <div>
+                <p className="error-text">{errorMessage}</p>
+              </div>)}
           </div>
           <h6 className="col-lg-10 mx-auto text-white mb-4">
             {" "}
