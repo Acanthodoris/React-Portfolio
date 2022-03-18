a0e6145d6b8f4039fab2fbfcf2ebe4073c2f9ec5
Becca <95663074+Acanthodoris@users.noreply.github.com>
2022-03-17 23:14:00 -0700

    resume pdf remove

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,28 +1,45 @@
 import React from "react";
 import "../../styles/Resume.css";
 import PDF from "../../assets/Resume.pdf";
-import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 // import { useNavigate } from "react-router-dom";
 
 function Resume() {
 
   return (
-    <section class="Resume">
+    <section className="bg-dark">
       <div className="container">
         <div className="row">
-          <div class="ResumeHome">
-            <div className="col-lg-10 mx-auto text-white mb-4 row">
-              <Document onLoadError={(err) => {console.log(err)}} file={PDF}>
-              <Page pageNumber={1} />
-              </Document>
-              <p className="text-center">
-                Download my{" "}
-                <a href={PDF} className="text-faded white-link" download>
-                  resume
-                </a>
-              </p>
-            </div>
+          <div className="col-lg-10 mx-auto text-white mb-4">
+            <h1 className="text-center">Resume</h1>
+            <hr className="light my-4" />
+            <h3>Front-end Proficiencies</h3>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>jQuery</li>
+              <li>Git</li>
+              <li>React</li>
+              <li>Bootstrap</li>
+              <li>Foundation</li>
+              <li>MUI</li>
+            </ul>
+            <h3>Back-end Proficiencies</h3>
+            <ul>
+              <li>APIs</li>
+              <li>Node</li>
+              <li>Express</li>
+              <li>MySQL, Sequelize</li>
+              <li>MongoDB, Mongoose</li>
+              <li>REST</li>
+              <li>MERN Stack</li>
+            </ul>
+            <p className="text-center">
+              Download my{" "}
+              <a href={PDF} className="text-faded white-link" download>
+                resume here.
+              </a>
+            </p>
           </div>
         </div>
       </div>
